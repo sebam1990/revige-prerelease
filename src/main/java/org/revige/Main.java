469b7c4186ff8e88378b1fb24a3fd4c6fddaac03
Sebastián Mariño <marinio.sebastian@gmail.com>
2025-05-24 15:32:44 -0300

    Initial commit. Main menu.

---
+++ src/main/java/org/revige/Main.java
@@ -0,0 +1,65 @@
+// Package
+
+    package org.revige;
+
+// Imports
+
+    import java.util.Scanner;
+
+// Class
+
+    public class Main {
+
+        // Main menu method
+
+            static void getOption(){
+
+                Scanner optionReader = new Scanner(System.in);
+                System.out.println(">> "); int menuOption = optionReader.nextInt();
+
+                if(menuOption == 1){
+
+                    System.out.println("Ingresaste como usuario administrador");
+
+                }
+
+                else if(menuOption == 2){
+
+                    System.out.println("Ingresaste como usuario cliente");
+
+                }
+
+                else if(menuOption == 3){
+
+                    System.exit(0);
+
+                }
+
+                else{
+
+                    System.out.println("La opción ingresada no es correcta. Volve a intentar.");
+                    getOption();
+
+                }
+
+            }
+
+        // Main method
+
+            public static void main(String[] args) {
+
+                System.out.println("REVIGE");
+                System.out.println("Red Virtual de Garages y Estacionamientos");
+                System.out.println("-----------------------------------------");
+                System.out.println("");
+
+                System.out.println("Selecciona una opción del menu para continuar");
+                System.out.println("1 - Ingresar como administrador");
+                System.out.println("2 - Ingresar como cliente");
+                System.out.println("3 - Salir");
+
+                getOption();
+
+            }
+
+        }
